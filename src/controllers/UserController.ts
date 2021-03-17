4ccc963d0f34abc78dfc7ce1f1518e7d5d3f71f5
erickLuizA <erickluiz69@gmail.com>
2021-03-17 10:57:37 -0300

    log errors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,7 +8,10 @@ import { User } from '../entities/user'
 import pool from '../config/config'
 
 export default class UserController {
-  async create (request: Request, response: Response): Promise<Response<any, Record<string, any>>> {
+  async create(
+    request: Request,
+    response: Response
+  ): Promise<Response<any, Record<string, any>>> {
     const { name, email, password }: Omit<User, 'id'> = request.body
 
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -21,11 +24,16 @@ export default class UserController {
 
       return response.status(200).send('User registered')
     } catch (error) {
+      console.log(error)
+
       return response.status(400).send()
     }
   }
 
-  async login (request: Request, response: Response): Promise<Response<any, Record<string, any>>> {
+  async login(
+    request: Request,
+    response: Response
+  ): Promise<Response<any, Record<string, any>>> {
     const { email, password }: Omit<User, 'id' | 'name'> = request.body
 
     const sql = 'SELECT * FROM users WHERE email = $1'
@@ -37,7 +45,10 @@ export default class UserController {
       const user: User = result.rows[0]
 
       if (await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign(user.id.toString(), process.env.ACCESS_TOKEN_SECRET ?? '')
+        const token = jwt.sign(
+          user.id.toString(),
+          process.env.ACCESS_TOKEN_SECRET ?? ''
+        )
 
         return response.json({ user: user.name, token })
       } else {
